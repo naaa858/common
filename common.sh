b9c5b68ffae93392ffac4e14a583a3043d333ae8
Namia-A <mk258855@outlook.com>
2024-09-17 02:06:58 +0800

    更新 common.sh

--- common.sh
+++ common.sh
@@ -321,8 +321,9 @@ mv -f uniq.conf feeds.conf.default
 cat >>"feeds.conf.default" <<-EOF
 src-git danshui1 https://github.com/Namia-A/openwrt-package.git;${SOURCE}
 src-git helloworld https://github.com/fw876/helloworld.git
+src-git passwall3 https://github.com/xiaorouji/openwrt-passwall-packages;main
 src-git namia https://github.com/Namia-A/package;main
-src-git https://github.com/ouyangzq/luci-app-cpe;main
+src-git cpe https://github.com/ouyangzq/luci-app-cpe;main
 
 
 EOF
